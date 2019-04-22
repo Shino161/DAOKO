98a6c4340aeda256168fe63ba853fbc1448b59e9
Shino161 <shino161@outlook.com>
2019-04-22 10:27:58 +0800

    fix: hide some info

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export default {
   baseUrl: {
-    dev: 'https://api.asoco.com.cn',
-    pro: 'http://api.iasoco.com'
+    dev: '',
+    pro: ''
   },
-  apiSucessCode: ['OK', 1000]
+  apiSucessCode: []
 
 }
